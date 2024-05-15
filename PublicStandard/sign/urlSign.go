abb29cceed263371648da48655fae0ab3362c38a
cyvk <2730962665@qq.com>
2024-05-15 12:28:03 +0800

    常规测试推送

--- PublicStandard/sign/urlSign.go
+++ PublicStandard/sign/urlSign.go
@@ -22,12 +22,12 @@ func UrlSign(Url *url.URL) string {
 }
 
 // StrToNum 字符串转数字
-// 将字符串转为字节流，然后将字节流的每个字节相加，最后除以9
+// 将字符串转为字节流，然后将字节流的每个字节相加
 func StrToNum(str string) int {
 	bytes := []byte(str)
 	t := 0
 	for _, b := range bytes {
 		t += int(b)
 	}
-	return t / 9
+	return t
 }
